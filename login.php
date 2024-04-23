029c9c9d208bdca1c3ce65429ddf5e7e6106b532
Rifqi Azhar Nugraha <rifqi.an@gmail.com>
2024-04-23 09:48:07 +0700

    Mengubah index.html dan menambahkan login.php

--- login.php
+++ login.php
@@ -3,10 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Login</title>
 </head>
 <body>
-    <h1>Selamat Datang!</h1>
-    <p>Ini adalah Web XI PPLG</p>
+    
 </body>
 </html>
